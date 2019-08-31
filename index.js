4606642d487cb858417beca50d61f6a521b06388
Harish Jangra <harish0212@yahoo.com>
2019-08-31 18:25:14 +0530

    new rn0.60.5

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 
 import { AppRegistry } from "react-native";
 import App from "./src";
+// import App from "./src605";
+
 import { name as appName } from "./app.json";
 // import "./debugmodules";
 
--- ios/easy_boiler/AppDelegate.m
+++ ios/easy_boiler/AppDelegate.m
@@ -6,7 +6,6 @@
  */
 
 #import "AppDelegate.h"
-#import <CodePush/CodePush.h>
 
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
@@ -36,7 +35,7 @@
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 #else
-  return [CodePush bundleURL];
+  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
 }
 
