90d53be9dca32f5a25f40f4d2f36c6f2b98e7abe
Harish Jangra <harish0212@yahoo.com>
2019-09-25 11:07:02 +0530

    rn61

--- src/App.js
+++ src/App.js
@@ -1,35 +1,34 @@
-import React from "react";
-import { StatusBar } from "react-native";
-import { StoreProvider } from "easy-peasy";
-import { Provider as PaperProvider } from "react-native-paper";
+import React from 'react';
+import {StatusBar} from 'react-native';
+import {StoreProvider} from 'easy-peasy';
+import {Provider as PaperProvider} from 'react-native-paper';
 
-import { APP_PREFIX } from "./Config/index";
+import {APP_PREFIX} from './Config/index';
 
-import NavigationService from "./Navigation";
-import createStore from "./Store";
-import PrimaryNav from "./Navigation/AppNavigation";
+import NavigationService from './Navigation';
+import createStore from './Store';
+import PrimaryNav from './Navigation/AppNavigation';
 
-import { ThemeProvider } from "./Themes/Context/ThemeContext";
-import { AppContextProvider } from "./Services/Auth/AppContext";
-import { LocaleContextProvider } from "./i18n/LocaleContext";
-import { NetInfoProvider } from "./Lib/NetInfo/Context";
+import {ThemeProvider} from './Themes/Context/ThemeContext';
+import {AppContextProvider} from './Services/Auth/AppContext';
+import {LocaleContextProvider} from './i18n/LocaleContext';
+import {NetInfoProvider} from './Lib/NetInfo/Context';
 
-import { Screen } from "./Components";
-import colors from "./Themes/Colors";
-import useTheme from "./Themes/Context";
-import useTranslation from "./i18n";
-
+import {Screen} from './Components';
+import useTheme from './Themes/Context';
+import useTranslation from './i18n';
+import {gestureHandlerRootHOC} from 'react-native-gesture-handler';
 //create the easy store
 const store = createStore();
 
 //return root component
-export default () => {
+const Root = () => {
   return (
     <Screen>
       <NetInfoProvider>
         <LocaleContextProvider>
           <StoreProvider store={store}>
-            <StatusBar translucent backgroundColor={"rgba(0,0,0,0.2)"} />
+            <StatusBar translucent backgroundColor={'rgba(0,0,0,0.2)'} />
             <ThemeProvider>
               <ThemeConsumer />
             </ThemeProvider>
@@ -41,18 +40,22 @@ export default () => {
 };
 
 const ThemeConsumer = props => {
-  const { theme } = useTheme();
-  const { t } = useTranslation();
+  const {theme} = useTheme();
+  const {t} = useTranslation();
 
   return (
     <PaperProvider theme={theme}>
       <AppContextProvider>
         <PrimaryNav
           uriPrefix={APP_PREFIX}
-          screenProps={{ theme, t }}
+          screenProps={{theme, t}}
           ref={nav => NavigationService.setTopLevelNavigator(nav)}
         />
       </AppContextProvider>
     </PaperProvider>
   );
 };
+
+//temp workaround for react-native-gesture-handler in react-native 0.61
+// take a look https://github.com/react-native-community/releases/issues/140#issuecomment-532819601
+export default gestureHandlerRootHOC(Root);
