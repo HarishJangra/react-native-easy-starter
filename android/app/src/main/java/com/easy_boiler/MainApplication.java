1f74946e7f803bf1d7e841fd7628f8d021bac976
Harish Jangra <harish0212@yahoo.com>
2020-04-25 16:28:37 +0530

    changes rn62

--- android/app/src/main/java/com/easy_boiler/MainApplication.java
+++ android/app/src/main/java/com/easy_boiler/MainApplication.java
@@ -11,6 +11,7 @@ import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.facebook.react.ReactInstanceManager;
 
 import java.util.List;
 
@@ -18,8 +19,6 @@ import androidx.multidex.MultiDex;
 import android.content.Context;
 import com.microsoft.codepush.react.CodePush;
 import java.lang.reflect.InvocationTargetException;
-
-
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -30,15 +29,16 @@ public class MainApplication extends Application implements ReactApplication {
     }
 
     @Override
-      protected String getJSBundleFile() {
-          return CodePush.getJSBundleFile();
-      }
+    protected String getJSBundleFile() {
+      return CodePush.getJSBundleFile();
+    }
 
     @Override
     protected List<ReactPackage> getPackages() {
       @SuppressWarnings("UnnecessaryLocalVariable")
       List<ReactPackage> packages = new PackageList(this).getPackages();
-      // Packages that cannot be autolinked yet can be added manually here, for example:
+      // Packages that cannot be autolinked yet can be added manually here, for
+      // example:
       // packages.add(new MyReactNativePackage());
       packages.add(new CodePush(BuildConfig.CODEPUSH_KEY, getApplicationContext(), BuildConfig.DEBUG));
       return packages;
@@ -54,9 +54,9 @@ public class MainApplication extends Application implements ReactApplication {
   public ReactNativeHost getReactNativeHost() {
     return mReactNativeHost;
   }
-  
+
   @Override
-  protected void attachBaseContext(Context base){
+  protected void attachBaseContext(Context base) {
     super.attachBaseContext(base);
     MultiDex.install(this);
   }
@@ -65,23 +65,29 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
-    // initializeFlipper(this); // Remove this line if you don't want Flipper enabled
+    initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+
   }
 
   /**
-   * Loads Flipper in React Native templates.
+   * Loads Flipper in React Native templates. Call this in the onCreate method
+   * with something like initializeFlipper(this,
+   * getReactNativeHost().getReactInstanceManager());
    *
    * @param context
+   * @param reactInstanceManager
    */
-  private static void initializeFlipper(Context context) {
+
+  private static void initializeFlipper(Context context, ReactInstanceManager reactInstanceManager) {
     if (BuildConfig.DEBUG) {
       try {
         /*
-         We use reflection here to pick up the class that initializes Flipper,
-        since Flipper library is not available in release mode
-        */
-        Class<?> aClass = Class.forName("com.facebook.flipper.ReactNativeFlipper");
-        aClass.getMethod("initializeFlipper", Context.class).invoke(null, context);
+         * We use reflection here to pick up the class that initializes Flipper, since
+         * Flipper library is not available in release mode
+         */
+        Class<?> aClass = Class.forName("com.easy_boiler.ReactNativeFlipper");
+        aClass.getMethod("initializeFlipper", Context.class, ReactInstanceManager.class).invoke(null, context,
+            reactInstanceManager);
       } catch (ClassNotFoundException e) {
         e.printStackTrace();
       } catch (NoSuchMethodException e) {
